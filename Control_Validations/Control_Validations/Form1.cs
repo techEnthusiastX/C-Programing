146cbfbe71482792cf08646f71cd88b4a5307b01
Harshad Bujare <108119821+techEnthusiastX@users.noreply.github.com>
2024-07-04 10:59:50 +0530

    Add files via upload

---
+++ Control_Validations/Control_Validations/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Control_Validations
+{
+    namespace WindowsFormsApp
+    {
+        public partial class MainForm : Form
+        {
+            public MainForm()
+            {
+                InitializeComponent();
+            }
+
+            private void MainForm_Load(object sender, EventArgs e)
+            {
+                // Initialize form controls and properties
+                InitializeControls();
+            }
+
+            private void InitializeControls()
+            {
+                // Add code to initialize controls (e.g., labels, textboxes, buttons) on the form
+            }
+
+            private void btnSubmit_Click(object sender, EventArgs e)
+            {
+                // Implement validation logic and handle submit button click event
+            }
+        }
+    }
+
+}
