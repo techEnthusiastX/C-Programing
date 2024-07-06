4f58281d7b6921e3da422dd77adbfa3c58998b45
Harshad Bujare <108119821+techEnthusiastX@users.noreply.github.com>
2024-07-06 18:35:59 +0530

    Add files via upload

---
+++ CRUDLibrary/CRUDLibrary/Form1.Designer.cs
@@ -0,0 +1,384 @@
+﻿namespace CRUDLibrary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.search = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnreset = new System.Windows.Forms.Button();
+            this.btmview = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btninsert = new System.Windows.Forms.Button();
+            this.returninfo = new System.Windows.Forms.TextBox();
+            this.description = new System.Windows.Forms.TextBox();
+            this.BookNo = new System.Windows.Forms.NumericUpDown();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textID = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider4 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider5 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BookNo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(104, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(820, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "JJMCOE STUDENT LIBRARY SYSTEM ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.btnreset);
+            this.groupBox1.Controls.Add(this.btmview);
+            this.groupBox1.Controls.Add(this.btndelete);
+            this.groupBox1.Controls.Add(this.btnupdate);
+            this.groupBox1.Controls.Add(this.btninsert);
+            this.groupBox1.Controls.Add(this.returninfo);
+            this.groupBox1.Controls.Add(this.description);
+            this.groupBox1.Controls.Add(this.BookNo);
+            this.groupBox1.Controls.Add(this.textName);
+            this.groupBox1.Controls.Add(this.textID);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(40, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(787, 355);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.search);
+            this.groupBox2.Controls.Add(this.txtSearch);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(49, 266);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(738, 76);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            // 
+            // search
+            // 
+            this.search.BackgroundImage = global::CRUDLibrary.Properties.Resources.download;
+            this.search.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.search.Location = new System.Drawing.Point(509, 19);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(46, 34);
+            this.search.TabIndex = 17;
+            this.search.UseVisualStyleBackColor = true;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(201, 19);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(258, 34);
+            this.txtSearch.TabIndex = 16;
+            this.txtSearch.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(52, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 26);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Search";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // btnreset
+            // 
+            this.btnreset.Location = new System.Drawing.Point(646, 206);
+            this.btnreset.Name = "btnreset";
+            this.btnreset.Size = new System.Drawing.Size(122, 40);
+            this.btnreset.TabIndex = 14;
+            this.btnreset.Text = "Reset";
+            this.btnreset.UseVisualStyleBackColor = true;
+            this.btnreset.Click += new System.EventHandler(this.btnreset_Click);
+            // 
+            // btmview
+            // 
+            this.btmview.Location = new System.Drawing.Point(646, 160);
+            this.btmview.Name = "btmview";
+            this.btmview.Size = new System.Drawing.Size(122, 40);
+            this.btmview.TabIndex = 13;
+            this.btmview.Text = "View";
+            this.btmview.UseVisualStyleBackColor = true;
+            this.btmview.Click += new System.EventHandler(this.btmview_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(646, 111);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(122, 40);
+            this.btndelete.TabIndex = 12;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(646, 64);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(122, 40);
+            this.btnupdate.TabIndex = 11;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btninsert
+            // 
+            this.btninsert.Location = new System.Drawing.Point(646, 15);
+            this.btninsert.Name = "btninsert";
+            this.btninsert.Size = new System.Drawing.Size(122, 40);
+            this.btninsert.TabIndex = 10;
+            this.btninsert.Text = "Insert";
+            this.btninsert.UseVisualStyleBackColor = true;
+            this.btninsert.Click += new System.EventHandler(this.btninsert_Click);
+            // 
+            // returninfo
+            // 
+            this.returninfo.Location = new System.Drawing.Point(225, 206);
+            this.returninfo.Name = "returninfo";
+            this.returninfo.Size = new System.Drawing.Size(379, 34);
+            this.returninfo.TabIndex = 9;
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(224, 160);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(380, 34);
+            this.description.TabIndex = 8;
+            // 
+            // BookNo
+            // 
+            this.BookNo.Location = new System.Drawing.Point(225, 111);
+            this.BookNo.Name = "BookNo";
+            this.BookNo.Size = new System.Drawing.Size(379, 34);
+            this.BookNo.TabIndex = 7;
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(225, 64);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(383, 34);
+            this.textName.TabIndex = 6;
+            // 
+            // textID
+            // 
+            this.textID.Location = new System.Drawing.Point(225, 15);
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(383, 34);
+            this.textID.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 26);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Return Info";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 26);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Description";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 26);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Book No.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 26);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Book Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Book ID";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CRUDLibrary.Properties.Resources.College_Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(833, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 234);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 476);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(987, 209);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            this.errorProvider3.ContainerControl = this;
+            // 
+            // errorProvider4
+            // 
+            this.errorProvider4.ContainerControl = this;
+            // 
+            // errorProvider5
+            // 
+            this.errorProvider5.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 708);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Library System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BookNo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnreset;
+        private System.Windows.Forms.Button btmview;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btninsert;
+        private System.Windows.Forms.TextBox returninfo;
+        private System.Windows.Forms.TextBox description;
+        private System.Windows.Forms.NumericUpDown BookNo;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.TextBox textID;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button search;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+        private System.Windows.Forms.ErrorProvider errorProvider4;
+        private System.Windows.Forms.ErrorProvider errorProvider5;
+    }
+}
+
---
+++ CRUDLibrary/CRUDLibrary/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.OleDb;
+
+namespace CRUDLibrary
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection(cs);
+            con.Open();
+            string query = "select * from Library where name like @name + '%'";
+            OleDbDataAdapter da = new OleDbDataAdapter(query, con);
+            da.SelectCommand.Parameters.AddWithValue("@name", txtSearch.Text.Trim());
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show("No Record Found");
+                dataGridView1.DataSource = null;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btninsert_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textID.Text) == true)
+            {
+                textID.Focus();
+                errorProvider1.SetError(this.textID, "Please Enter ID");
+            }
+            else if (string.IsNullOrEmpty(textName.Text) == true)
+            {
+                textName.Focus();
+                errorProvider2.SetError(this.textName, "Please Enter Name");
+            }
+            else if (string.IsNullOrEmpty(BookNo.Text) == true)
+            {
+                BookNo.Focus();
+                errorProvider3.SetError(this.BookNo, "Please Enter Book Number");
+            }
+            else if (string.IsNullOrEmpty(description.Text) == true)
+            {
+                description.Focus();
+                errorProvider4.SetError(this.description, "Please Enter Book Description");
+            }
+            else if (string.IsNullOrEmpty(returninfo.Text) == true)
+            {
+                returninfo.Focus();
+                errorProvider5.SetError(this.returninfo, "Please Enter Book return details");
+            }
+            else
+            {
+                errorProvider1.Clear();
+                errorProvider2.Clear();
+                errorProvider3.Clear();
+                errorProvider4.Clear();
+                errorProvider5.Clear();
+
+                OleDbConnection con = new OleDbConnection(cs);
+                con.Open();
+
+                string query2 = "select * from Library where id=@id";
+                OleDbCommand cmd2 = new OleDbCommand(query2, con);
+                cmd2.Parameters.AddWithValue("@id", textID.Text);
+                OleDbDataReader dr = cmd2.ExecuteReader();
+                if (dr.HasRows == true)
+                {
+                    MessageBox.Show(textID.Text + " ID has already taken...!!");
+                }
+                else
+                {
+                    string query = "insert into Library values(@id, @name, @number, @description, @returninfo)";
+                    OleDbCommand cmd = new OleDbCommand(query, con);
+                    cmd.Parameters.AddWithValue("@id", textID.Text);
+                    cmd.Parameters.AddWithValue("@name", textName.Text);
+                    cmd.Parameters.AddWithValue("@number", BookNo.Value);
+                    cmd.Parameters.AddWithValue("@description", description.Text);
+                    cmd.Parameters.AddWithValue("@returninfo", returninfo.Text);
+
+                    int a = cmd.ExecuteNonQuery();
+                    if (a > 0)
+                    {
+                        MessageBox.Show("Record Saved Succesfully", "SUCCESS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        display();
+                        reset();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    con.Close();
+                }
+            }
+        }
+
+        void reset()
+        {
+            textID.Clear();
+            textName.Clear();
+            BookNo.Value = 0;
+            description.Clear();
+            returninfo.Clear();
+            textID.Focus();
+        }
+        void display()
+        {
+            OleDbConnection con = new OleDbConnection(cs);
+            con.Open();
+            string query = "select * from Library";
+            OleDbDataAdapter da = new OleDbDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btnreset_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void btmview_Click(object sender, EventArgs e)
+        {
+            display();
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            textID.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            textName.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            BookNo.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            description.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            returninfo.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection(cs);
+            con.Open();
+
+            string query = "UPDATE Library set id=@id, name=@name, number=@number, description=@description, returninfo=@returninfo WHERE id=@id";
+            OleDbCommand cmd = new OleDbCommand(query, con);
+            cmd.Parameters.AddWithValue("@id", textID.Text);
+            cmd.Parameters.AddWithValue("@name", textName.Text);
+            cmd.Parameters.AddWithValue("@number", BookNo.Value);
+            cmd.Parameters.AddWithValue("@description", description.Text);
+            cmd.Parameters.AddWithValue("@returninfo", returninfo.Text);
+
+            int a = cmd.ExecuteNonQuery();
+            if (a > 0)
+            {
+                MessageBox.Show("Record updated Succesfully", "SUCCESS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                display();
+                reset();
+            }
+            else
+            {
+                MessageBox.Show("Failed", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            con.Close();
+        }
+        
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection(cs);
+            con.Open();
+
+            string query = "delete from Library where id=@id";
+            OleDbCommand cmd = new OleDbCommand(query, con);
+            cmd.Parameters.AddWithValue("@id", textID.Text);
+            
+
+            int a = cmd.ExecuteNonQuery();
+            if (a > 0)
+            {
+                MessageBox.Show("Record Deleted Succesfully", "SUCCESS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                display();
+                reset();
+            }
+            else
+            {
+                MessageBox.Show("Failed", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            con.Close();
+        }
+
+        private void search_Click(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection(cs);
+            con.Open();
+            string query = "select * from Library where name like @name + '%'";
+            OleDbDataAdapter da = new OleDbDataAdapter(query, con);
+            da.SelectCommand.Parameters.AddWithValue("@name", txtSearch.Text.Trim());
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show("No Record Found");
+                dataGridView1.DataSource = null; 
+            }
+        }
+    }
+}
