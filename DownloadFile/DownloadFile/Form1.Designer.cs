ffc48dddedbe12dee97c40a5749031c6e858f61b
Harshad Bujare <108119821+techEnthusiastX@users.noreply.github.com>
2024-07-09 22:46:35 +0530

    Add files via upload

---
+++ DownloadFile/DownloadFile/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace DownloadFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(433, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Download and Process File";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(76, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(425, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter the URL of the file to download";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(66, 191);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(441, 22);
+            this.txtURL.TabIndex = 2;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownload.Location = new System.Drawing.Point(164, 246);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(232, 46);
+            this.btnDownload.TabIndex = 3;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(73, 342);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(433, 32);
+            this.progressBar.TabIndex = 4;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(76, 389);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(68, 25);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 576);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ DownloadFile/DownloadFile/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+
+namespace DownloadFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        WebClient client;
+        string filename;
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string url = txtURL.Text;
+
+            if (!string.IsNullOrEmpty(url))
+            {
+                Thread thread = new Thread(() =>
+                    {
+                        Uri uri = new Uri(url);
+                        filename = System.IO.Path.GetFileName(uri.AbsolutePath);
+                        client.DownloadDataAsync(uri, Application.StartupPath + "/" + filename);
+                    });
+                thread.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new WebClient();
+            client.DownloadProgressChanged += Client_DownloadProgressChanged;
+            client.DownloadFileCompleted += Client_DownloadFileCompleted;
+        }
+        private void Client_DownloadFileCompleted(object? sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Completed !", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            string dir = Path.GetDirectoryName(Application.StartupPath + "/" + filename);
+            System.Diagnostics.Process.Start("explorer",dir);
+
+        }
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+ 
+        }
+    }
+}
