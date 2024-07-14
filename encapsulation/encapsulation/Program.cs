51e745870ce871f3e224b6c7628086937d562583
Harshad Bujare <108119821+techEnthusiastX@users.noreply.github.com>
2024-07-14 14:24:45 +0530

    Add files via upload

---
+++ encapsulation/encapsulation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace encapsulation
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Harshad";
+            person.Age = 20;
+
+            Console.WriteLine("Name: " + person.Name);
+            Console.WriteLine("Age: " + person.Age);
+
+            Console.ReadLine();
+        }
+    }
+
+    
+}
