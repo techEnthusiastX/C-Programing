7e640f5dd183df77f777a2202186afa0be7a0564
Harshad Bujare <108119821+techEnthusiastX@users.noreply.github.com>
2024-07-24 20:39:31 +0530

    Add files via upload

---
+++ extensionmethod/extensionmethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace extensionmethod
+{
+       static class StringExtensions
+    {
+        public static string Reverse(this string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello, World!";
+            string reversed = text.Reverse();
+            Console.WriteLine(reversed);
+
+            Console.ReadLine();
+        }
+    }
+
+}
