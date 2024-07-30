6eb9a2f87e264e25b2db161b0c5e2c99788dac09
Harshad Bujare <108119821+techEnthusiastX@users.noreply.github.com>
2024-07-30 10:44:30 +0530

    Add files via upload

---
+++ functionoverloading/functionoverloading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace functionoverloading
+{
+    class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            int result1 = calculator.Add(5, 10);
+            double result2 = calculator.Add(2.5, 3.7);
+
+            Console.WriteLine("Result 1: " + result1);
+            Console.WriteLine("Result 2: " + result2);
+
+            Console.ReadLine();
+        }
+    }
+
+}
