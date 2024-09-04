b8f4bb0f81bb864c9ff3f64bc78c0c06d09bed83
Harshad Bujare <108119821+techEnthusiastX@users.noreply.github.com>
2024-09-04 18:28:40 +0530

    Add files via upload

---
+++ hybridinheritance/hybridinheritance/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hybridinheritance
+{
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating");
+        }
+    }
+
+    interface ISwim
+    {
+        void Swim();
+    }
+
+    interface IFly
+    {
+        void Fly();
+    }
+
+    class Dolphin : Animal, ISwim
+    {
+        public void Swim()
+        {
+            Console.WriteLine("Dolphin is swimming");
+        }
+    }
+
+    class Bird : Animal, IFly
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Bird is flying");
+        }
+    }
+
+    class FlyingFish : Animal, ISwim, IFly
+    {
+        public void Swim()
+        {
+            Console.WriteLine("Flying fish is swimming");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Flying fish is flying");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolphin dolphin = new Dolphin();
+            dolphin.Eat();
+            dolphin.Swim();
+
+            Bird bird = new Bird();
+            bird.Eat();
+            bird.Fly();
+
+            FlyingFish flyingFish = new FlyingFish();
+            flyingFish.Eat();
+            flyingFish.Swim();
+            flyingFish.Fly();
+
+            Console.ReadLine();
+        }
+    }
+
+}
